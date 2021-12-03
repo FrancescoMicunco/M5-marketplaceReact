5cc32313acfed193ae3a66ad47f1513bee4d3430
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-03 10:24:26 +0100

    structure

---
+++ marketplace/src/Components/Backoffice.jsx
@@ -0,0 +1,5 @@
+const Backoffice = () => {
+  console.log("this is the backpoffice");
+};
+
+export default Backoffice;
---
+++ marketplace/src/Components/Homepage.jsx
@@ -0,0 +1,5 @@
+const Homepage = () =>{
+ console.log("this is the home page")
+}
+
+export default Homepage
