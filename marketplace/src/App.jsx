572f7365b4741b0bb3fe26c2cf8dff587d187bcd
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-03 13:29:49 +0100

    structure route on app.js

--- marketplace/src/App.jsx
+++ marketplace/src/App.jsx
@@ -3,16 +3,22 @@ import MyNavbar from './Components/Navbar'
 import Backoffice from './Components/Backoffice';
 import Homepage from './Components/Homepage'
 import 'bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
-    return ( <div className = "App" >
-        
-        
-        <MyNavbar />
-            <Homepage />
-            <Backoffice />
-         </div>
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <MyNavbar />
+        </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/" element={<Backoffice />}></Route>
+        </Routes>
+
+       
+      </div>
     );
 }
 
