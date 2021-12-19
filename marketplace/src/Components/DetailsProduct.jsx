ee6c262be8432f39505423bb5486e961bb2d3da5
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-19 12:43:04 +0100

    detailProduct

--- marketplace/src/Components/DetailsProduct.jsx
+++ marketplace/src/Components/DetailsProduct.jsx
@@ -1,19 +1,18 @@
-
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const DetailsProduct = () => {
   const { productId } = useParams();
-const [pDet, setPDet] = useState(null)
+  const [pDet, setPDet] = useState(null);
+  const [quantity, setQuantity] = useState(0);
 
   const getDetail = async () => {
     try {
       const res = await fetch(`http://localhost:3001/product/${productId}`);
       if (res.ok) {
         const data = await res.json();
-        console.log(data);
-        setPDet(data)
+        setPDet(data);
       }
     } catch (error) {
       console.log("server error!");
@@ -25,24 +24,36 @@ const [pDet, setPDet] = useState(null)
   }, []);
 
   return (
-  <Container>
+    <Container>
       <Row>
-          {
-              pDet &&
-              <>
-              <Col md="6">
-              <img src={pDet.image} alt={pDet.name}/>
-              </Col>
-              <Col md="6">
-                  <h2>{pDet.name}</h2>
-                  <p>{pDet.description}</p>
-                  <p>{pDet.price}</p>
-              </Col>
-              </>
-          }
+        {pDet && (
+          <>
+            <Col md="3">
+              <img src={pDet.image} alt={pDet.name} className="img-fluid mb-2" />
+            </Col>
+            <Col md="9">
+              <h2>{pDet.name}</h2>
+              <p>{pDet.description}</p>
+              <p>$ {pDet.price}</p>
+            </Col>
+
+            <Col md="2">
+              <Button
+                onClick={() => {
+                  let q = quantity + 1;
+                  setQuantity(q);
+                  console.log("The quantity is actually: ", quantity);
+                }}
+                variant="success"
+              >
+                Add to cart
+              </Button>{" "}
+            </Col>
+          </>
+        )}
       </Row>
-  </Container>
-  )
+    </Container>
+  );
 };
 
 export default DetailsProduct;
