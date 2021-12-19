5067e5b562d384e684dbc3069625e58ed872791a
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-19 21:55:35 +0100

    created shopping cart

--- marketplace/src/Components/Cart.jsx
+++ marketplace/src/Components/Cart.jsx
@@ -1,20 +1,51 @@
 import React from "react";
 import { Container, Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
-const Cart = ({props})=>{
+const Cart = () => {
+  const [productId, setProductId] = useState("");
+  const [quantity, setQuantity] = useState("");
 
+  const getCart = async () => {
+    try {
+      const res = await fetch("http://localhost:3001/cart");
+      if (res.ok) {
+        const data = await res.json();
+const pId=data.productId
+console.log(pId)
+const quan=data.quantity
+        setProductId(data.productId);
+          setQuantity(data.quantity);
+        console.log("those are the values", productId, quantity)
+        } else {
+          console.log("error in the cart")
+        }
+      
+    } catch (error) {}
+  };
 
-    return (
-      <Container>
-        <h2>This is You Shopping Cart</h2>
-        <h3>
-          {props.name} quantity 
-        </h3>
-        <Button variant="outline-primary">add</Button>
-        <Button variant="outline-primary">reduce</Button>
-        <Button variant="outline-danger">delete</Button>
-      </Container>
-    );
-}
+  useEffect(() => {
+    getCart();
+  }, []);
 
-export default Cart
+  useEffect(() => {
+    return;
+  }, [quantity]);
+
+  return (
+    <Container>
+      {
+        <>
+          <h2>This is You Shopping Cart {quantity} </h2>
+          
+
+          {/* <Button variant="outline-primary">add</Button>
+            <Button variant="outline-primary">reduce</Button>
+            <Button variant="outline-danger">delete</Button> */}
+        </>
+      }
+    </Container>
+  );
+};
+
+export default Cart;
--- marketplace/src/Components/DetailsProduct.jsx
+++ marketplace/src/Components/DetailsProduct.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
-import Cart from './Cart'
+import Cart from "./Cart";
 
 const DetailsProduct = () => {
   const { productId } = useParams();
@@ -14,36 +14,34 @@ const DetailsProduct = () => {
       if (res.ok) {
         const data = await res.json();
         setPDet(data);
+        console.log(pDet);
       }
     } catch (error) {
       console.log("server error!");
     }
   };
 
-  const addToCart = async ({}) =>{
-    try {
-      const res = await fetch(`http://localhost:3001/product/${productId}`, 
-      {method: "PUT",
-        headers:{ 'Content-type': 'text/html'},
-      body:JSON.stringify()
-    });
-      if (res.ok) {
-        const data = await res.json();
-        setPDet(data);
-      }
-    } catch (error) {
-      console.log("server error!");
-    }
-  }
-
-  const reduceToCart = async () =>{}
-
-
-
-
   useEffect(() => {
     getDetail();
   }, []);
+
+  const modifyCart = async () => {
+    console.log("this is the object", pDet)
+    console.log("this is the quantity", quantity)
+    const objectToAdd = {pDet, quantity}
+    console.log(objectToAdd)
+    try {
+      const res = await fetch(`http://localhost:3001/cart`, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(objectToAdd),
+      });
+          } catch (error) {
+      console.log("server error!");
+    }
+  };
+
+  
 
   return (
     <Container>
@@ -67,26 +65,34 @@ const DetailsProduct = () => {
             <Col md="2">
               <Button
                 onClick={() => {
+                  alert("are you shure?")
                   let q = quantity + 1;
+                  console.log(q)
                   setQuantity(q);
-                  console.log("The quantity is actually: ", quantity);
+                  console.log(quantity)
+                  modifyCart();
+                  
                 }}
                 variant="success"
               >
                 Add to cart
               </Button>
-
+            </Col>
+            <Col md="2">
               <Button
                 onClick={() => {
-                  let q = quantity -1;
-                  setQuantity(q);
-                  console.log("The quantity is actually: ", quantity);
+                  if (quantity <= 0) {
+                    alert("This product is not in the cart");
+                  } else {
+                    let q = quantity - 1;
+                    setQuantity(q);
+                    modifyCart()
+                  }
                 }}
                 variant="success"
               >
                 Reduce to cart
               </Button>
-              <Cart quantity={quantity} props={pDet} />
             </Col>
           </>
         )}
